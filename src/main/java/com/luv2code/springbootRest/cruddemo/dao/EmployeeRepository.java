3f36cf7bc838c3e93295585d8b6449788ff5f2cb
Misagh62 <misoli.fash@gmail.com>
2025-03-05 14:15:26 +0100

    Spring JpaRepository

--- src/main/java/com/luv2code/springbootRest/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/springbootRest/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.luv2code.springbootRest.cruddemo.dao;
 
 import com.luv2code.springbootRest.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee , Integer> {
 
 }
